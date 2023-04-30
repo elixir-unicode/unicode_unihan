1af8db8fb596655547dc8296c5099954a336ac33
Kip Cole <kipcole9@gmail.com>
2023-04-30 16:50:50 +0800

    Clean out some crufty files

--- benchee/regex.exs
+++
@@ -1,5 +0,0 @@
-Benchee.run(%{
-  "Unicode"  => fn -> Unicode.Property.uppercase?("A") end,
-  "unicode regex" => fn -> Regex.match?(~r/\p{Lu}/u, "A") end,
-  "regex" => fn -> Regex.match?(~r/[A-Z]/u, "A") end
-  })
